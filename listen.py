01df04343479ee7b6e0af6a36ae3426a80cdb323
SSANGYOON <sanyoonlee479@gmail.com>
2022-06-07 11:43:48 +0000

    [Feature] merged feature_extraction

--- listen.py
+++ listen.py
@@ -1,10 +1,18 @@
 from typing import List
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile,Response
 from pydantic import BaseModel
+from ePillID_benchmark.epillid_src.models.embedding_model import EmbeddingModel
+import torch
+from torchvision.transforms import transforms
 import uvicorn
 import numpy as np
-import base64
+import cv2
+from rembg import remove
 import pymysql
+from PIL import Image
+from io import BytesIO
+import pandas as pd
+import os
 
 conn = None
 cursor = None
@@ -20,52 +28,111 @@ class ResponseItem(BaseModel):
 class ItemOut(BaseModel):
     items: List[ResponseItem] = []
 
+res_mean = [0.485, 0.456, 0.406]
+res_std = [0.229, 0.224, 0.225]
+
+torch_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(res_mean, res_std)
+])
 
 app = FastAPI()
+df = None
+model = EmbeddingModel('resnet50')
+def subimage(image, rect):
+    theta = rect[2]-90
+    center = (int(rect[0][0]),int(rect[0][1]))
+    height = int(rect[1][0])
+    width = int(rect[1][1])
+    theta *= 3.14159 / 180 # convert to rad
+    v_x = (np.cos(theta), np.sin(theta))
+    v_y = (-np.sin(theta), np.cos(theta))
+    s_x = center[0] - v_x[0] * ((width-1) / 2) - v_y[0] * ((height-1) / 2)
+    s_y = center[1] - v_x[1] * ((width-1) / 2) - v_y[1] * ((height-1) / 2)
 
+    mapping = np.array([[v_x[0],v_y[0], s_x],
+                        [v_x[1],v_y[1], s_y]])
+
+    cropped = cv2.warpAffine(image,mapping,(width, height),flags=cv2.WARP_INVERSE_MAP,borderMode=cv2.BORDER_REPLICATE)
+    if cropped.shape[0] > cropped.shape[1]:
+        cropped = cv2.rotate(cropped,cv2.ROTATE_90_COUNTERCLOCKWISE)
+    cropped = cv2.resize(cropped,(224,int(224*cropped.shape[0]/cropped.shape[1])))
+    zero_image = np.zeros((224,224,3),dtype=np.uint8)
+    zero_image[112-cropped.shape[0]//2 : 112 +(cropped.shape[0]-cropped.shape[0]//2),:] = cropped
+    return zero_image
+
+def segment_image(img_arr):
+    if img_arr.shape[-1] == 4:
+        img_arr = cv2.cvtColor(img_arr,cv2.COLOR_RGBA2RGB)
+    imgray = cv2.cvtColor(img_arr, cv2.COLOR_BGR2GRAY)
+    ret, th = cv2.threshold(imgray, 10,255,cv2.THRESH_BINARY)
+
+    # 컨튜어 찾기
+    contours, _ = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contr = contours[0]
+    rect = cv2.minAreaRect(contr)
+    return subimage(img_arr,rect)
 
 @app.on_event("startup")
 def startup_event():
-    global conn
-    global cursor
-    conn = pymysql.connect(
-        user="sangyoon", passwd="1234", host="localhost", db="pills", charset="utf8"
-    )
-    cursor = conn.cursor()
+    global model
+    global df
+    df = pd.read_csv('OpenData_PotOpenTabletIdntfcC20220601.csv')
+    model.load_state_dict(torch.load('ePillID_benchmark/pill_encoder.pt'))
+    model = model.eval()
+    print('load weight done!')
+    #global conn
+    #global cursor
+    #conn = pymysql.connect(
+    #    user="sangyoon", passwd="1234", host="localhost", db="pills", charset="utf8"
+    #)
+    #cursor = conn.cursor()
 
 
 @app.on_event("shutdown")
 def shutdown_event():
-    conn.commit()
-    conn.close()
+    pass
+    #conn.commit()
+    #conn.close()
 
+@app.post("/image_segment/")
+async def query(file : UploadFile = File(...)):
+    img = Image.open(BytesIO(await file.read()))
+    img = remove(img)
+    arr = np.array(img)
+    seg = segment_image(arr)
+    
+    ret = Image.fromarray(seg)
+    with BytesIO() as buf:
+        ret.save(buf, format='PNG')
+        im_bytes = buf.getvalue()
+    headers = {'Content-Disposition': 'inline; filename="test.png"'}
+    return Response(im_bytes, headers=headers, media_type='image/png')
+
+def cosine_distance(a,b):
+    dis = np.dot(a,b)/(np.linalg.norm(a)*np.linalg.norm(b))
+    return dis
 
 @app.post("/image_query/")
-def query(files: List[UploadFile] = File(...)):
+async def query(files: List[UploadFile] = File(...)):
 
-    sql = """
-        SELECT  품목명,큰제품이미지,성상, feature
-        from pills_table
-        where 색상앞 = '연두'
-        and 의약품제형 = '원형'
-    """
-
-    cursor.execute(sql)
-    res = cursor.fetchall()
+    feats =[]
+    feat_dir = 'ePillID_benchmark/features'
+    for file in files:
+        imgarr = np.array(Image.open(BytesIO(await file.read())))
+        feats.append(model(torch_transform(imgarr).unsqueeze(0)).squeeze().detach().numpy())
     items = ItemOut()
-    random_feature = np.random.rand(1024)
-    for i in range(min(5, len(res))):
-        name = res[i][0]
-        image_url = res[i][1]
-        shape = res[i][2]
-        feat = np.frombuffer(
-            base64.b64decode(res[i][3].encode("utf-8")), dtype=np.float64
-        )
-        score = np.dot(feat, random_feature) / (
-            (np.dot(feat, feat) * np.dot(random_feature, random_feature)) ** 0.5
-        )
+    l = []
+    for index, row in df.iterrows():
+        feat_item = np.load(os.path.join(feat_dir,str(row['품목일련번호']) + '.npy'))
+        l.append((max(cosine_distance(feats[0],feat_item[1])+cosine_distance(feats[1],feat_item[0]),
+                    cosine_distance(feats[0],feat_item[0])+cosine_distance(feats[1],feat_item[1]),
+        )/2,row['큰제품이미지'],row['품목명'],row['분류명']))
+    l.sort(reverse = True)
+    print(l[:10])
+    for i in range(10):
         items.items.append(
-            {"name": name, "image_url": image_url, "shape": shape, "score": score}
+            {"name": l[i][2], "image_url": l[i][1], "usage": l[i][3], "score": l[i][0]}
         )
     return items
 
--- main.py
+++ main.py
@@ -15,11 +15,13 @@ def main():
     st.sidebar.title("사용법")
     st.sidebar.subheader("1. 알아보고 싶은 알약의 앞면과 뒷면이 잘 보이도록 사진을 각각 한 장씩 찍습니다.")
     st.sidebar.subheader("2. 찍은 알약의 앞면 사진을 상단에, 뒷면 사진을 하단에 업로드합니다.")
-    st.sidebar.subheader("3. 앞면 사진과 뒷면 사진을 업로드하였다면 아래의 버튼을 눌러줍니다.")
-    st.sidebar.subheader("4. 업로드한 사진과 유사한 알약을 k개까지 웹페이지 상으로 보여줍니다.")
+    st.sidebar.subheader("3. 알약 영역이 잘 분리되었는지 확인해줍니다.")
+    st.sidebar.subheader("4. 사진이 잘 분리되었다면 예측 시작 버튼을 누릅니다.")
+    st.sidebar.subheader("5. 업로드한 사진과 유사한 알약을 k개까지 웹페이지 상으로 보여줍니다.")
 
     col1, col2 = st.columns(2)
     col3, col4 = st.columns(2)
+    col5, col6 = st.columns(2)
     uploaded_file_front = col1.file_uploader(
         "알약 앞면 사진을 올려주세요.", type=["jpg", "jpeg", "png"]
     )
@@ -28,6 +30,11 @@ def main():
         image_bytes = uploaded_file_front.getvalue()
         image = Image.open(io.BytesIO(image_bytes))
         col3.image(image, caption="Pill front")
+        res = requests.post('http://127.0.0.1:8080/image_segment', files = {'file':image_bytes})
+        seg_front = res.content
+        seg_front_img = Image.open(io.BytesIO(seg_front))
+        col5.image(seg_front_img, caption='Pill front')
+
 
     uploaded_file_back = col2.file_uploader(
         "알약 뒷면 사진을 올려주세요.", type=["jpg", "jpeg", "png"]
@@ -37,13 +44,17 @@ def main():
         image_bytes = uploaded_file_back.getvalue()
         image = Image.open(io.BytesIO(image_bytes))
         col4.image(image, caption="Pill back")
+        res = requests.post('http://127.0.0.1:8080/image_segment', files = {'file':image_bytes})
+        seg_back = res.content
+        seg_back_img = Image.open(io.BytesIO(seg_back))
+        col6.image(seg_back_img, caption='Pill back')
 
     if uploaded_file_front and uploaded_file_back:
         if st.button("예측 시작"):
 
             files = [
-                ("files", uploaded_file_front.getvalue()),
-                ("files", uploaded_file_back.getvalue()),
+                ("files", seg_front),
+                ("files", seg_back),
             ]
             res = requests.post("http://127.0.0.1:8080/image_query/", files=files)
 
@@ -53,12 +64,12 @@ def main():
             cols = st.columns(outn)
             for i in range(outn):
                 output = outputs[i]
-                response = requests.get(output["image_url"])
+                response = requests.get(output['image_url'])
                 img = Image.open(io.BytesIO(response.content))
                 cols[i].write(f"{i+1}. {output['name']}")
                 cols[i].image(img)
-                cols[i].write(f"성상: {output['shape']}")
-                cols[i].write(f"점수: {output['score']}")
+                cols[i].write(f"효능: {output['usage']}")
+                cols[i].write(f"유사도: {output['score']}")
 
 
 main()
